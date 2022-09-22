9a41ba7ef79b1a8af5c63730b721eaeb954c0336
Marshall Paider <paidermarshall@gmail.com>
2022-09-22 09:47:43 -0500

    Mde better

--- main.py
+++ main.py
@@ -1,5 +1,43 @@
-import math
+def encryptMessage(message, depth):
+    encryptedMessage = ''
+    firstMod = determineFirstRowMod(depth)
+    mod = firstMod
 
+    for i in range(int(depth)):
+        # This block of code accounts for the "peaks" of the fence
+        if mod == firstMod:
+            # add characters that are in [0] equivalence class (mod mod) to encrypted message and replace with spaces in the original message
+            # replace with spaces to not mess up the indexing
+            for j in range(0, len(message)):
+                if j % mod == 0:
+                    encryptedMessage += message[j]
+                    message = replace_char_at_index(message, j, ' ')
+            # squish the word back together
+            message = message.replace(" ", "")
+            # subtract 1 from the mod since peaks only have 1 letter ???
+            mod -= 1
+        # This block of code accounts for the middle section of the fence
+        elif mod != 1:
+            for j in range(0, len(message)):
+                # look for characters at the negative sloped part of fence
+                # add characters that are in [0] equivalence class (mod mod) to encrypted message and replace with spaces in the original message
+                if j % mod == 0:
+                    encryptedMessage += message[j]
+                    message = replace_char_at_index(message, j, ' ')
+                # checks if there is a corresponding positive sloped part of the fence
+                # add characters that are in [mod - 1] === [-1] equivalence class (mod mod) to encrypted message and replace with spaces in the original message
+                if mod != firstMod and j != 1 and j % mod == mod - 1:
+                    encryptedMessage += message[j]
+                    message = replace_char_at_index(message, j, ' ')
+            # squish word back together
+            message = message.replace(" ", "")
+            # move onto the next mod needed for the next middle row
+            mod -= 2
+        # This block of code accounts for the "valleys" of the fence
+        # There is nothing left of the string to split so add it to the encrypted message
+        else:
+            encryptedMessage += message
+    return encryptedMessage
 def replace_char_at_index(org_str, index, replacement):
     ''' Replace character at index in string org_str with the
     given replacement character.'''
@@ -8,65 +46,13 @@ def replace_char_at_index(org_str, index, replacement):
         new_str = org_str[0:index] + replacement + org_str[index + 1:]
     return new_str
 
-def encryptDecryptMessage(choice, message, key):
+def encryptDecryptMessage(choice, message, depth):
+    output = ''
     if choice.upper() == 'E':
-        # twoDArray = []
-        # encryptedMessage = ''
-        #
-        # for i in range(int(key)):
-        #     twoDArray.append([])
-        #
-        # for i in range(int(key)):
-        #     for j in range(len(message)):
-        #         twoDArray[i].insert(j, -1)
-        #
-        # firstCountBy = determineFirstCountBy(key)
-        # countBy = firstCountBy
-        #
-        # for i in range(int(key)):
-        #     if countBy != 0:
-        #         for j in range(0, math.floor(len(message) / int(countBy))):
-        #             encryptedMessage += message[j*countBy]
-        #             #message[j*countBy] = " "
-        #             message = replace_char_at_index(message, j*countBy, ' ')
-        #             if countBy != firstCountBy and j*countBy + 1 < len(message) and j != 0:
-        #                 encryptedMessage += message[j * countBy + 1]
-        #                 #message[j*countBy] = " "
-        #                 message = replace_char_at_index(message, j * countBy + 1, ' ')
-        #         message = message.replace(" ", "")
-        #         countBy -= 2
-        #     else:
-        #         encryptedMessage += message
-        encryptedMessage = ''
-        firstCountBy = determineFirstCountBy(key)
-        countBy = firstCountBy
+        output = encryptMessage(message, depth)
+    return output
 
-        for i in range(int(key)):
-            if countBy == firstCountBy:
-                for j in range(0, len(message)):
-                    if j % countBy == 0:
-                        encryptedMessage += message[j]
-                        message = replace_char_at_index(message, j, ' ')
-                message = message.replace(" ", "")
-                countBy -= 1
-            elif countBy != 1:
-                for j in range(0, len(message)):
-                    if j % countBy == 0:
-                        encryptedMessage += message[j]
-                        message = replace_char_at_index(message, j, ' ')
-                    if countBy != firstCountBy and j != 1 and j % countBy == countBy - 1:
-                        encryptedMessage += message[j]
-                        message = replace_char_at_index(message, j, ' ')
-                message = message.replace(" ", "")
-                countBy -= 2
-            else:
-                encryptedMessage += message
-
-
-
-        # print(twoDArray)
-        return encryptedMessage
-def determineFirstCountBy(depth):
+def determineFirstRowMod(depth):
     return 2*(int(depth) - 1)
 
 def isAlphabetic(string):
